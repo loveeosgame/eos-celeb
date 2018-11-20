75c543033723107edfcac7e8b9b102874dcc928f
jin10086 <1019855680@qq.com>
2018-11-20 12:59:09 +0800

    .

--- contract/eoscrazytown/eoscrazytown.cpp
+++ contract/eoscrazytown/eoscrazytown.cpp
@@ -6,7 +6,7 @@ void eoscrazytown::init()
     require_auth(_self);
 
     auto g = _bagsglobal.get_or_create(_self, bagsglobal{
-                                                  .pool = 0, .team = 0, .last = N(crazytown.bp), .st = now() + 60 * 60 * 11, .ed = now() + 60 * 60 * 11 + 60 * 60 * 24});
+                                                  .pool = 0, .team = 0, .last = N(gy2dgmztgqge), .st = now() + 60 * 60 * 11, .ed = now() + 60 * 60 * 11 + 60 * 60 * 24});
     _bagsglobal.set(g, _self);
 }
 // @abi action
@@ -16,15 +16,15 @@ void eoscrazytown::clear()
     _bagsglobal.remove();
 }
 
-void eoscrazytown::newbag(account_name &from, asset &eos)
+void eoscrazytown::newbag(asset &eos)
 {
 
     require_auth(_self);
     for (int i = 0; i < 57; ++i)
     {
-        bags.emplace(from, [&](auto &p) {
+        bags.emplace(_self, [&](auto &p) {
             p.id = bags.available_primary_key();
-            p.owner = from;
+            p.owner = N(eosbocai3333);
             p.price = eos.amount;
         });
     }
@@ -41,6 +41,15 @@ void eoscrazytown::setslogan(account_name &from, uint64_t id, string memo)
     });
 }
 
+bool isbot(uint8_t id, uint64_t amount, uint64_t checksum)
+{
+    if (id * amount + 1 == checksum)
+    {
+        return false;
+    }
+    return true;
+}
+
 // input
 void eoscrazytown::onTransfer(account_name &from, account_name &to, asset &eos, string &memo)
 {
@@ -50,62 +59,43 @@ void eoscrazytown::onTransfer(account_name &from, account_name &to, asset &eos, 
     require_auth(from);
     eosio_assert(eos.is_valid(), "Invalid token transfer...");
     eosio_assert(eos.symbol == EOS_SYMBOL, "only EOS token is allowed");
-    eosio_assert(eos.amount > 0, "must buy a positive amount");
 
     if (memo.substr(0, 3) == "buy")
     {
-
         memo.erase(0, 4);
         std::size_t s = memo.find(' ');
         if (s == string::npos)
         {
             s = memo.size();
         }
-
         auto id = string_to_price(memo.substr(0, s));
-        //  auto id = 0;
         memo.erase(0, s + 1);
         auto itr = bags.find(id);
         eosio_assert(itr != bags.end(), "no character exist");
-        eosio_assert(eos.amount >= itr->next_price(), "no enough eos");
-        asset d(eos.amount - itr->next_price(), EOS_SYMBOL);
+        eosio_assert(eos.amount == itr->next_price(), "pls check amount");
+        auto chceksum = string_to_price(memo.c_str());
+        print("checksum:::", chceksum);
 
-        if (d.amount > 0)
+        if (isbot(id, eos.amount, chceksum))
         {
-            action( // winner winner chicken dinner
-                permission_level{_self, N(active)},
-                TOKEN_CONTRACT, N(transfer),
-                make_tuple(_self, from, d,
-                           std::string("refund")))
-                .send();
+            return;
         }
 
-        d.amount = itr->next_price() - itr->price;
-        ;
-
-        auto g = _bagsglobal.get_or_create(_self, bagsglobal{
-
-                                                      .pool = 0, .team = 0});
-
-        g.team += d.amount * 1 / 1000; //0.1 %
-        g.pool += d.amount * 10 / 100; //10%
+        auto g = _bagsglobal.get_or_default();
+        g.team += eos.amount * 1 / 100;  //1 %
+        g.pool += eos.amount * 10 / 100; //10%
         g.last = from;
-        g.ed = now() + 60 * 60 * 24;
-
+        g.ed = now() + 60 * 10;
         _bagsglobal.set(g, _self);
 
-        auto delta = d;
-        delta.amount = d.amount * 899 / 1000; //89.9
-
-        if (delta.amount > 0)
-        {
-            action( // winner winner chicken dinner
-                permission_level{_self, N(active)},
-                N(eosio.token), N(transfer),
-                make_tuple(_self, itr->owner, delta,
-                           std::string("next hodl")))
-                .send();
-        }
+        auto delta = eos;
+        delta.amount = eos.amount * 89 / 100; //89%
+        action(                               // winner winner chicken dinner
+            permission_level{_self, N(active)},
+            N(eosio.token), N(transfer),
+            make_tuple(_self, itr->owner, delta,
+                       std::string("next hodl")))
+            .send();
 
         bags.modify(itr, 0, [&](auto &t) {
             t.owner = from;
