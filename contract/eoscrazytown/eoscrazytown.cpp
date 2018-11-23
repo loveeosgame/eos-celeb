078762a8d7f0e4e7c4d877363022a955b98c2e5f
jin10086 <1019855680@qq.com>
2018-11-23 21:36:32 +0800

    go

--- contract/eoscrazytown/eoscrazytown.cpp
+++ contract/eoscrazytown/eoscrazytown.cpp
@@ -9,8 +9,8 @@ void eoscrazytown::init()
     g.pool = 0;
     g.team = 0;
     g.last = N(gy2dgmztgqge);
-    g.st = now();
-    g.ed = now() + 60;
+    g.st = 1542981600;
+    g.ed = 1542981600 + 60;
     _bagsglobal.set(g, _self);
 }
 // @abi action
