2e13ce3ff33652ed482c66e75e6242cb123653dc
jin10086 <1019855680@qq.com>
2018-12-05 18:32:26 +0800

    Update eoscrazytown.sh

--- contract/eoscrazytown.sh
+++ contract/eoscrazytown.sh
@@ -5,6 +5,6 @@ cleos set contract eosbocaihero eoscrazytown -p eosbocaihero
 
 cleos push action eosbocaihero init '[]' -p eosbocaihero 
 
-cleos push action eosbocaihero newbag '["1.0000 ALLIN",0]' -p eosbocaihero 
-cleos push action eosbocaihero newbag '["1.0000 ALLIN",57]' -p eosbocaihero 
+cleos push action eosbocaihero newbag '["0.1000 EOS",0]' -p eosbocaihero 
+cleos push action eosbocaihero newbag '["0.1000 EOS",57]' -p eosbocaihero 
 
